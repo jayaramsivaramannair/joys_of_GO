3d699a2db97c7f9e59af0a1537607269f3755b92
Jayaram Nair <jayaramsivaramannair@gmail.com>
2021-11-26 18:23:23 -0500

    Added a code to showcase use of value receivers method for a struct in Go

---
+++ structs/value_receivers.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+)
+
+const usixteenbitmax float64 = 65535
+const kmh_multiple float64 = 1.60934
+
+//Structs are analogous to classes in other object oriented languages
+type car struct {
+	gas_pedal uint16
+	brake_pedal uint16 
+	steering_wheel int16
+	top_speed_kmh float64
+}
+
+//This method is available for call on the car struct - A Value receivers method
+func (c car) kmh() float64 {
+	return float64(c.gas_pedal) * (c.top_speed_kmh / usixteenbitmax)
+}
+
+func (c car) mph() float64 {
+	return float64(c.gas_pedal) * (c.top_speed_kmh / usixteenbitmax /kmh_multiple)
+}
+
+func main() {
+	a_car := car {gas_pedal: 65000, brake_pedal: 0, steering_wheel: 12561, top_speed_kmh: 225.0}
+
+	fmt.Println(a_car.gas_pedal)
+	fmt.Println(a_car.kmh())
+	fmt.Println(a_car.mph())
+
+
+}
