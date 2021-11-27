91c3715fc8bafcf18d2b3d5ba046599e7bd83f1a
Jayaram Nair <jayaramsivaramannair@gmail.com>
2021-11-27 07:46:02 -0500

    Added the code to visit each location in site map struct and extract the information into news struct

--- web_app/aggregate.go
+++ web_app/aggregate.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"encoding/xml"
+	"net/http"
+	"io/ioutil"
 )
 
 var washPostXML = []byte (`
@@ -21,26 +23,27 @@ var washPostXML = []byte (`
 
 type SitemapIndex struct {
 	//The data type is a slice of location
-	Locations []Location `xml:"sitemap"`
+	Locations [] string `xml:"sitemap>loc"`
 }
 
-
-type Location struct {
-	Loc string `xml:"loc"`
-}
-
-
-func(l Location) String() string {
-	return fmt.Sprintf(l.Loc)
+type News struct {
+	Titles []string `xml:"url>news>title"`
+	Keywords []string `xml:"url>news>Keywords"`
+	Locations []string `xml:"url>loc"`
 }
 
 func main() {
 	//resp, _ := http.Get("https://www.washingtonpost.com/news-sitemap-index.xml")
 	bytes := washPostXML
 	var s SitemapIndex
+	var n News
 	xml.Unmarshal(bytes, &s)
 	//fmt.Println(s.Locations)
 	for _, Location := range s.Locations {
-		fmt.Printf("\n%s", Location)
+		resp, _ := http.Get(Location)
+		bytes, _ := ioutil.ReadAll(resp.Body)
+		xml.Unmarshal(bytes, &n)
 	}
+
+	fmt.Println(n)
 }
