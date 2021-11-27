f087f03dd66a07ab212c5913b3318b8fdf8f917e
Jayaram Nair <jayaramsivaramannair@gmail.com>
2021-11-27 08:10:15 -0500

    Added the code to create a map structure for information extracted from each url

--- web_app/aggregate.go
+++ web_app/aggregate.go
@@ -28,8 +28,13 @@ type SitemapIndex struct {
 
 type News struct {
 	Titles []string `xml:"url>news>title"`
-	Keywords []string `xml:"url>news>Keywords"`
+	Keywords []string `xml:"url>news>keywords"`
 	Locations []string `xml:"url>loc"`
+}
+
+type NewsMap struct {
+	Keyword string
+	Location string
 }
 
 func main() {
@@ -37,13 +42,24 @@ func main() {
 	bytes := washPostXML
 	var s SitemapIndex
 	var n News
+
+	news_map := make(map[string]NewsMap)
+
 	xml.Unmarshal(bytes, &s)
 	//fmt.Println(s.Locations)
 	for _, Location := range s.Locations {
 		resp, _ := http.Get(Location)
 		bytes, _ := ioutil.ReadAll(resp.Body)
 		xml.Unmarshal(bytes, &n)
+		for idx, _ := range n.Titles {
+			news_map[n.Titles[idx]] = NewsMap{n.Keywords[idx], n.Locations[idx]}
+		}
 	}
 
-	fmt.Println(n)
+	for idx, data := range news_map {
+		fmt.Println("\n\n\n", idx)
+		fmt.Println("\n", data.Keyword)
+		fmt.Println("\n", data.Location)
+
+	}
 }
