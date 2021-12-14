d68f38efd1e9fdfd94018594a960fec0e49793e8
Jayaram Nair <jayaramsivaramannair@gmail.com>
2021-12-13 19:48:56 -0500

    Implemented the updateMovie function to be used when accessing PUT route

--- project_CRUD-API/main.go
+++ project_CRUD-API/main.go
@@ -75,6 +75,28 @@ func createMovie(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(movie)
 }
 
+
+func updateMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	params := mux.Vars(r)
+
+	for index, item := range movies {
+		if item.ID == params["id"] {
+			//First delete the movie which needs to be updated
+			movies = append(movies[:index], movies[index+1:]...)
+			//Create a new movie variable
+			var movie Movie
+			_ = json.NewDecoder(r.Body).Decode(&movie)
+			//Set the ID field on the new movie struct to be the parameter passed in the query string
+			movie.ID = params["id"]
+			movies = append(movies, movie)
+			json.NewEncoder(w).Encode(movie)
+			return 
+		}
+	}
+}
+
 func main() {
 	//Helps create a new router which available on the mux package
 	r := mux.NewRouter()
