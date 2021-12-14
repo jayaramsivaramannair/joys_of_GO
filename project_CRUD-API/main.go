13736fc1c880c3d7876db59aea4ef2fc592f8a54
Jayaram Nair <jayaramsivaramannair@gmail.com>
2021-12-13 19:11:56 -0500

    Populated the movies slice with data for 2 movies

--- project_CRUD-API/main.go
+++ project_CRUD-API/main.go
@@ -24,15 +24,31 @@ type Director struct{
 
 var movies []Movie
 
+//http.ResponseWriter is used to send the response back to the client
+//http.Request is used to process the incoming request from the client
+func getMovies(w http.ResponseWriter, r *http.Request) {
+
+}
+
 func main() {
 	//Helps create a new router which available on the mux package
 	r := mux.NewRouter()
+
+
+	movies = append(movies, Movie{ID: "1", Isbn: "438227", Title : "Movie One", Director : &Director{FirstName: "John", LastName: "Doe"}})
+	movies = append(movies, Movie{ID: "2", Isbn: "45455", Title : "Movie Two", Director : &Director{FirstName: "Steve", LastName: "Smith"}})
 
 	r.HandleFunc("/movies", getMovies).Methods("GET")
 	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
 	r.HandleFunc("/movies", createMovie).Methods("POST")
 	r.HandleFunc("movies/{id}", updateMovie).Methods("PUT")
 	r.HandleFunc("movies/{id}", deleteMovie).Methods("DELETE")
+
+	fmt.Printf("Starting server at port 8000\n")
+
+	//Listen and Serve function is used to start the server
+	log.Fatal(http.ListenAndServe(":8000", r))
+
 
 }
 
