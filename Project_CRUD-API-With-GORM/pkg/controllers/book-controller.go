67096d7d7033060c54160546bf613c32ecb1df3e
Jayaram Nair <jayaramsivaramannair@gmail.com>
2021-12-15 20:11:40 -0500

    Added the code for GetBookById function in controllers folder

--- Project_CRUD-API-With-GORM/pkg/controllers/book-controller.go
+++ Project_CRUD-API-With-GORM/pkg/controllers/book-controller.go
@@ -21,3 +21,20 @@ func GetBook(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Write(res)
 }
+
+
+func GetBookById(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	bookId := vars.["bookId"]
+	ID, err := strconv.ParseInt(bookId, 0,0)
+	if err != nil {
+		fmt.Println("error while parsing")
+	}
+
+	//underscore is used since we do not want to use the db value returned from GetBookById function
+	bookDetails, _ := models.GetBookById(ID)
+	res, _ := json.Marshal(bookDetails)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
