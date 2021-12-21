b233d4f9b48022416ef531a77ac953bd8e292d51
Jayaram Nair <jayaramsivaramannair@gmail.com>
2021-12-21 05:33:56 -0500

    Implemented code for Authenticate function in middleware folder

---
+++ Project_JWT-Authentication/middleware/authMiddleware.go
@@ -0,0 +1,22 @@
+package middleware
+
+import (
+	"fmt"
+	"net/http"
+	helper "github.com/jayaramsivaramannair/joys_of_GO/Project_JWT-Authentication/helpers"
+	"github.com/gin-gonic/gin"
+)
+
+func Authenticate() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		clientToken := c.Request.Header.Get("token")
+		if clientToken == "" {
+			c.JSON(http.StatusInternalServerError, gin.H{"error" : fmt.Sprintf("No Authorization header provided")})
+			c.Abort()
+			return 
+		}
+
+		claims, err := helper.ValidateToken(clientToken)
+		
+	}
+}
