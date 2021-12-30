762265df837ec03a420600347705c79626548ef6
Jayaram Nair <jayaramsivaramannair@gmail.com>
2021-12-30 04:28:08 -0500

    Added the code for GetLatestBlogTitles function to scrape titles for blog posts

---
+++ Project_Scraper-With-Goquery/main.go
@@ -0,0 +1,43 @@
+package main
+
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"github.com/PuerkitoBio/goquery"
+)
+
+
+func main() {
+	blogTitles, err := GetLatestBlogTitles("https://golangcode.com")
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	fmt.Prinln("Blog Titles:")
+	fmt.Printf(blogTitles)
+
+}
+
+func GetLatestBlogTitles(url string)(string , error) {
+	resp, err := http.Get(url)
+
+	if err != nil {
+		return "", err
+	}
+
+	doc, err := goquery.NewDocumentFromReader(resp.body)
+
+	if err != nil {
+		return "", err
+	}
+
+	titles := ""
+	doc.Find(".post-title").Each(func(i int, s *goquery.Selection) {
+		titles += "-" + s.Text() + "\n"
+	})
+
+	return titles, nil 
+}
