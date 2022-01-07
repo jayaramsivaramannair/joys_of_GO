bbed9304993ebba7f08718060aa6d41514a18c47
Jayaram Nair <jayaramsivaramannair@gmail.com>
2022-01-06 19:47:57 -0500

    Added code to simulate a relay race using 4 goroutines and an unbuffered channel

---
+++ goroutines_channels/unbuffered-channel/relay-race.go
@@ -0,0 +1,69 @@
+//Unbuffered channel to simulate a relay race between four goroutines. 
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+//wg is used to wait for the program to finish
+var wg sync. WaitGroup
+
+func main() {
+	//Create an unbuffered channel. 
+	baton := make(chan int)
+
+	//Add a count of one for the last runner. 
+	wg.Add(1)
+
+
+	//First runner to his mark. 
+	go Runner(baton)
+
+
+	//Start the race. 
+	baton <- 1
+
+	//Wait for the race to finish. 
+	wg.Wait()
+}
+
+
+//Runner simulates a person running in the relay race. 
+func Runner(baton chan int) {
+	var newRunner int
+
+	//Wait to receive the baton
+	runner := <- baton
+
+
+	//Start running around the track 
+	fmt.Printf("Runner %d Running with Baton\n", runner)
+
+
+	//New Runner to the line
+	if runner != 4 {
+		newRunner = runner + 1
+		fmt.Printf("Runner %d To The Line\n", newRunner)
+		go Runner(baton)
+	}
+
+	//Running around the track. 
+	time.Sleep(100 * time.Millisecond)
+
+	//Is the race over. 
+	if runner == 4 {
+		fmt.Printf("Runner %d Finished, Race Over\n", runner)
+		//Signals to the main function that we are done with the goroutine
+		wg.Done()
+		return 
+	}
+
+
+	//Exchange the baton for the next runner. 
+	fmt.Printf("Runner %d Exchange With Runner %d\n", runner, newRunner)
+
+
+	baton <- newRunner
+}
